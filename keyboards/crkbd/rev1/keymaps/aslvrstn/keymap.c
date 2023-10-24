2c26e46668b95531d4a1af43e330137e394dbe85
Alex <aslvrstn@gmail.com>
2023-10-24 03:24:52 -0400

    remove busted config

--- keyboards/crkbd/rev1/keymaps/aslvrstn/keymap.c
+++ keyboards/crkbd/rev1/keymaps/aslvrstn/keymap.c
@@ -84,10 +84,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   // https://www.reddit.com/r/olkb/comments/njb4o9/esc_key_with_backtick_on_layer_and_tilde_on/gz6mo8i/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
   switch (keycode) {
